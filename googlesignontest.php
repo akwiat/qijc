cb66eced2b852091df3eb949c5c438d5823cd510
akwiat <na>
2019-12-03 02:19:48 -0700

    same file

--- googlesignontest.php
+++ googlesignontest.php
@@ -1,5 +1,26 @@
 <?php
-    // some php stuff
+require_once 'vendor/autoload.php';
+ini_set('session.use_only_cookies', 1); //Deters session hijacking, if I understand.
+session_start();
+
+if(isset($_POST['token']) && !isset($_SESSION['username'])) {
+  $CLIENT_ID = "139584810606-lpct333ou0h73fm6fc1dqnj9fs9om2b6.apps.googleusercontent.com";
+  $t = $_POST['token'];
+  echo "received token";
+  $client = new Google_Client(['client_id' => $CLIENT_ID]);  // Specify the CLIENT_ID of the app that accesses the backend
+  $payload = $client->verifyIdToken($t);
+  echo "reached ";
+  if ($payload) {
+    echo "payload: ";
+    echo $payload;
+    $userid = $payload['sub'];
+    // If request specified a G Suite domain:
+    //$domain = $payload['hd'];
+  } else {
+    echo "invalid";
+  // Invalid ID token
+}
+}
 ?>
 <html lang="en">
   <head>
