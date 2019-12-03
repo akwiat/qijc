6e51b96c410273f78f80f7aa25043bc59e32966a
akwiat <na>
2019-12-02 21:29:22 -0700

    db init script

---
+++ initdatabase.py
@@ -0,0 +1,19 @@
+import sys
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  passwd=sys.argv[1]
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("CREATE DATABASE d2")
+mycursor.execute("USE d1")
+mycursor.execute("CREATE TABLE users (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), gid VARCHAR(255))")
+sql = "INSERT INTO users (name, gid) VALUES (%s, %s)"
+val = ("alex", "asdfasf")
+mycursor.execute(sql, val)
+mydb.commit()
+print(mycursor.rowcount, "record inserted.")
