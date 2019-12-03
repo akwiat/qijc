25b4a3e107d7ed6f63f0b76f5ee321232f723bbb
akwiat <na>
2019-12-03 00:54:13 -0700

    semi

--- start.php
+++ start.php
@@ -5,7 +5,7 @@ session_start();
 
 if(isset($_POST['token']) && !isset($_SESSION['username'])) {
   $CLIENT_ID = "139584810606-lpct333ou0h73fm6fc1dqnj9fs9om2b6.apps.googleusercontent.com";
-  $t = $_POST['token']
+  $t = $_POST['token'];
   echo "received token";
   $client = new Google_Client(['client_id' => $CLIENT_ID]);  // Specify the CLIENT_ID of the app that accesses the backend
   $payload = $client->verifyIdToken($id_token);
@@ -17,7 +17,7 @@ if(isset($_POST['token']) && !isset($_SESSION['username'])) {
     // If request specified a G Suite domain:
     //$domain = $payload['hd'];
   } else {
-    echo "invalid"
+    echo "invalid";
   // Invalid ID token
 }
 }
